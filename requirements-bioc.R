9528837361bda4b6ea527913fc7db80b5b55ffc1
Jonathan Talbot-Martin <jtalbotmartin@outlook.com>
2024-05-24 19:12:28 +0100

    installation of scater

--- requirements-bioc.R
+++ requirements-bioc.R
@@ -1,6 +1,7 @@
 bioc_pkgs<-c(
-"doMC", "doRNG","doSNOW", "mixtools", "SummarizedExperiment", "AUCell", "SingleCellExperiment", "scater", "variancePartition"
+"doMC", "doRNG","doSNOW", "mixtools", "SummarizedExperiment", "AUCell", "SingleCellExperiment", "variancePartition"
 )
 
 requireNamespace("BiocManager")
-BiocManager::install(bioc_pkgs,ask=F)
+BiocManager::install(bioc_pkgs,ask=FALSE)
+BiocManager::install("scater", ask=FALSE)
